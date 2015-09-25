263d7ac031d3b4183fa71a1e886e34bb50f95abf
Jennifer Rospad <jlr247@mail.usask.ca>
2015-09-25 00:28:44 -0600

    Have written code to read entire line of input, loop back, and exit successfully using some suggested methods from the class forums. Previous code still present in comments in case we want to reuse it or go back to what we had.

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include "string.h"
 #include "unistd.h"
 
-void exec(char** word_list, int word_count){
-    char** args;
-    char* cmd;
+/*void exec(char** word_list, int word_count){
+    char** args;	// Arguments accompanying the command
+    char* cmd;		// Command to execute
     int* pipe_indexes = (int *) malloc(word_count * sizeof(int));
     int pipe_count = 0;
     int i, j, k, return_value;
@@ -35,12 +35,12 @@ void exec(char** word_list, int word_count){
 
 }
 // compile code with (-g enables gdb debugging)
-//gcc -Wall -pedantic -g -o main main.c
+//gcc -Wall -pedantic -g -o main main.c */
 
 
 int odd_shell(){
 
-    char c = '\0';
+    /* char c = '\0';
     char *word = (char *) malloc(128 * sizeof(char));
     char** word_list = (char **) malloc(128 * sizeof(sizeof(char)));
     int count = 0;
@@ -50,7 +50,7 @@ int odd_shell(){
     while(c != EOF) {
         c = getchar();
         switch(c) {
-            case '\n': /* parse and execute. */
+            case '\n': // parse and execute.
                 // add last word to list
                 word_list[count] = word;
                 count++;
@@ -67,16 +67,38 @@ int odd_shell(){
                 word_length=0;
                 break;
             default:
-/*                asprintf(&word, "%s%c", word, c);*/
+                asprintf(&word, "%s%c", word, c);
                 word[word_length] = c;
                 word_length++;
                 break;
         }
     }
-    return 0;
+    exit(EXIT_SUCCESS);*/
+    
+    // Allocate space to save input from command line
+    // Max 150 characters chosen
+    char* input_str = malloc(150 * sizeof(char));
+    
+    // Continue prompting until user types "exit"
+    while(1){
+    	printf("osh> ");
+    	fgets( input_str, 150, stdin );
+    	
+    	// Check if stdin was an exit message
+    	if(strcmp( input_str, "exit\n" ) == 0){
+    		return EXIT_SUCCESS;
+    	}
+    	
+    	// Do work with the input
+    	printf("The input was: %s", input_str);
+    }
+    
+    // Return because error has occurred if this
+    // code has been reached.
+    return EXIT_FAILURE;   
 }
 
-int main(int argc, char **argv, char **envp) {
+int main(int argc, char* argv[]) {
     return odd_shell();
 }
 
