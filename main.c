c37f7cb028a8506c96b70747a06298f17b1ef960
Jennifer Rospad <jlr247@mail.usask.ca>
2015-09-26 15:34:32 -0600

    The program will now read and execute a single command entered, and then loop back to the osh> prompt. Known issue: must type exit more than once to leave shell in some circumstances. This is probably a problem with fork() that needs to be addressed.

--- main.c
+++ main.c
@@ -75,40 +75,70 @@ int odd_shell(){
     }
     exit(EXIT_SUCCESS);*/
     
-    // Allocate space to save input from command line
-    char* input_str;
-    size_t max_length = 128;
-    size_t nchar_read;
+    size_t max_str_length = 128; /* max length of command typed after osh prompt */
+    int max_num_args = 64; /* max number of words types after osh prompt */
     
     // Continue prompting until user types "exit"
     while(1){
     	printf("osh> ");
-    	input_str = ( char* ) malloc( max_length * sizeof(char) );
-    	nchar_read = getline( &input_str, &max_length, stdin );
+    	
+    	// Get command line input
+    	char* input_str = ( char* ) malloc( max_str_length * sizeof(char) );
+    	size_t nchar_read = getline( &input_str, &max_str_length, stdin );
     	    	   	
-    	// Check if stdin was an exit message
+    	// Check if command was an exit message
     	if(strcmp( input_str, "exit\n" ) == 0){
     		return EXIT_SUCCESS;
     	}
     	
-    	// Check for getline error
+    	// Check for getline() error
     	if( -1 == nchar_read ){
     		return EXIT_FAILURE;
     	}
     	
-    	// Do work with the input 
-    	// *** HAVING TROUBLE FIGURING OUT STRTOK: It only takes in
-    	// the first word in the command line!   	
+    	// Delete /n character at end of line
+    	size_t length = (strlen(input_str) -1);
+    	input_str[length] = '\0';
+    	
+    	// Get all words from command line into an array of words
 		char* cmd_args = strtok (input_str, " ");
-		//execvp( cmd, &input_str );
-		printf("The cmd is: %s and the args are:\n", cmd_args);
-		while(cmd_args != NULL)
+		int i = 0;
+		char* wordArray[max_num_args];
+		while((cmd_args != NULL) && (i < max_num_args))
 		{
-			printf("%s\n", cmd_args);
+			wordArray[i] = malloc(strlen(cmd_args) + 1);
+			strcpy(wordArray[i], cmd_args);
 			cmd_args = strtok(NULL, " ");
-		}
+			i += 1;
+		}	
 		
+		// ** TEST CODE ** to test the that array saved correctly:    
+    	//i -= 1; // Decrement i to the highest index number
+    	//printf("The words in reverse order are:\n");
+    	//for ( ; i > -1 ; i--){
+    	//	printf("---%s----\n", wordArray[i]);
+    	//}
+    	
+    	int rc = fork();
+    	if ( -1 == rc ) {
+    		printf("Error in forking.");
+    		return EXIT_FAILURE;
+    	}
+    	else if ( 0 == rc ) { // Child process to execute
+    		execvp(wordArray[0], wordArray);
+    	}
+    	else { //Parent process waits for child to complete
+    		wait(NULL);
+    	}
+    	
+    	// Free allocated memory
+    	free (input_str);
+    	i -= 1; // Decrement i to the highest index number
+    	for( ; i > -1; i--){
+    		free (wordArray[i]);
+    	}
     }
+
     
     // Return because error has occurred if this
     // code has been reached.
