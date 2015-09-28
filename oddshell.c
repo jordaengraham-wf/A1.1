13b45a978079fc38d3d00801ae051838c957bfc7
Jordaen Graham <jordaen.graham@workiva.com>
2015-09-28 15:24:12 -0600

    added collecting pipes

--- oddshell.c
+++ oddshell.c
@@ -3,12 +3,42 @@
 #include "string.h"
 #include "unistd.h"
 
+int execute_pipes(char **wordArray, int wordArray_length){
+    int j, childPID, return_value;
+
+    childPID = fork();
+    if ( -1 == childPID ) {
+        printf("Error in forking.\n");
+        return EXIT_FAILURE;
+    }
+    else if ( 0 == childPID ) { /* Child process to execute */
+        printf("Im a child! (PID: %d)\n", getpid());
+        printf("CMD: %s, Args: ", wordArray[0]);
+        for(j = 1; j < wordArray_length; j++ )
+            printf("%s, ", wordArray[j]);
+        printf("\n");
+
+        if(execvp(wordArray[0], wordArray) == -1){
+            printf("Execution failed: CMD: %s, Args: ", wordArray[0]);
+            for(j = 1; j < wordArray_length -1; j++)
+                printf("%s, ", wordArray[j]);
+            printf("%s\n", wordArray[j]);
+            printf("Child %d exited\n", getpid());
+            exit(1);
+        }
+    }
+    else { /* Parent process waits for child to complete */
+        wait(NULL);
+        printf("I'm the parent! (PID: %d)\n", getpid());
+    }
+}
+
 int odd_shell(){    
     char *input_str, *cmd_args;
-    char **wordArray, **cmdArray;
+    char **wordArray, **pipesArray;
     size_t max_str_length = 128; /* max length of command typed after osh prompt */
     ssize_t nchar_read, length;
-    int wordArray_index, wordArray_length, j, rc, return_value, cmd_count = 1, max_num_args = 64; /* max number of words types after osh prompt */
+    int wordArray_length, pipes_count, max_num_args = 64; /* max number of words types after osh prompt */
 
     /* Continue prompting until user types "exit" */
     while(1){
@@ -27,63 +57,57 @@ int odd_shell(){
     	length = (strlen(input_str) -1);
     	input_str[length] = '\0';
 
-    	/* Get all words from command line into an array of words */
-		cmd_args = strtok (input_str, " ");
-        wordArray = (char **) malloc(max_num_args * sizeof(char));
-        cmdArray  = (char **) malloc(max_num_args * sizeof(char));
-        wordArray_length = 0;
+        /* get pipes */
+        cmd_args = strtok (input_str, "|");
+        pipesArray = (char **) malloc(max_num_args * sizeof(char));
+        pipes_count = 0;
 
-		while((cmd_args != NULL) && (wordArray_length < max_num_args))
-		{
-			wordArray[wordArray_length] = malloc(strlen(cmd_args) + 1);
-			strcpy(wordArray[wordArray_length], cmd_args);
-			cmd_args = strtok(NULL, " ");
-			wordArray_length += 1;
-		}
-
-        if (wordArray_length == 0) /* handle blank line entered */
-            continue;
-
-        wordArray_index = wordArray_length - 1;
-
-        /* Check if command was an exit message */
-        if(strcmp( wordArray[0], "exit" ) == 0){
-            return EXIT_SUCCESS;
+        while((cmd_args != NULL) && (pipes_count < max_num_args))
+        {
+            pipesArray[pipes_count] = malloc(strlen(cmd_args) + 1);
+            strcpy(pipesArray[pipes_count], cmd_args);
+            cmd_args = strtok(NULL, "|");
+            pipes_count += 1;
         }
 
-        /* Parse by pipes */
-        
+        for (int k = pipes_count-1; k >= 0; k--) {
+            cmd_args = strtok (pipesArray[k], " ");
+            wordArray = (char **) malloc(max_num_args * sizeof(char));
+            wordArray_length = 0;
 
-    	rc = fork();
-    	if ( -1 == rc ) {
-    		printf("Error in forking.\n");
-    		return EXIT_FAILURE;
-    	}
-    	else if ( 0 == rc ) { /* Child process to execute */
-    		printf("Im a child! (PID: %d)\n", getpid());
-            printf("CMD: %s, Args: ", wordArray[0]);
-            for(j=1; j < wordArray_length; j++ )
-                printf("%s, ", wordArray[j]);
-            printf("\n");
-            if(execvp(wordArray[0], wordArray) == -1){
-                printf("Execution failed: CMD: %s, Args: ", wordArray[0]);
-                for(j=1; j< wordArray_length -1; j++)
-                    printf("%s, ", wordArray[j]);
-                printf("%s\n", wordArray[j]);
-                printf("Child %d exited\n", getpid());
-                exit(1);
-    	    }
+            while((cmd_args != NULL) && (wordArray_length < max_num_args))
+            {
+                wordArray[wordArray_length] = malloc(strlen(cmd_args) + 1);
+                strcpy(wordArray[wordArray_length], cmd_args);
+                cmd_args = strtok(NULL, " ");
+                wordArray_length += 1;
+            }
+            if (wordArray_length == 0) /* handle blank line entered */
+                continue;
+
+            /* Check if command was an exit message */
+            if(strcmp( wordArray[0], "exit" ) == 0){
+                return EXIT_SUCCESS;
+            }
+
+//            int pipefd[2];
+//            pipe(pipefd);
+//            dup2(pipefd[0], 0)
+
+//            dup2(pipefd[1], 1)
+            execute_pipes(wordArray, wordArray_length);
+
+            for( wordArray_length = wordArray_length - 1; wordArray_length > -1; wordArray_length--){
+                free (wordArray[wordArray_length]);
+            }
         }
-    	else { /* Parent process waits for child to complete */
-    		wait(NULL);
-    		printf("I'm the parent! (PID: %d)\n", getpid());
-    	}
 
     	/* Free allocated memory */
     	free (input_str);
-    	wordArray_length = wordArray_length - 1; /* Decrement i to the highest index number */
-    	for( ; wordArray_length > -1; wordArray_length--){
-    		free (wordArray[wordArray_length]);
+        /* Decrement i to the highest index number */
+
+    	for( pipes_count = pipes_count - 1; pipes_count > -1; pipes_count--){
+    		free (pipesArray[pipes_count]);
     	}
     }
 
